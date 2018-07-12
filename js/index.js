8be463bfe8b77979c369e8afc9c0d4d85b05f4d4
HuaLouChuQiu <1015110571@qq.com>
2018-07-12 15:46:02 +0800

    1

--- js/index.js
+++ js/index.js
@@ -79,6 +79,13 @@ $(function(){
    
     var userEmail = sessionStorage.getItem("email");
     console.log(userEmail);
+    if(userEmail != undefined){
+        $("#name-name").html(userEmail);
+        $("#name").css("display","block");
+        $("#goin1").css("display","none");
+    }else{
+        console.log("kong")
+    }
 
     // 登录的表单
     $("#loginForm").validate({
