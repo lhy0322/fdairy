2d30dcb18831b8719cb43f3520b635b5325de3b9
HuaLouChuQiu <1015110571@qq.com>
2018-07-13 10:29:23 +0800

    aa

--- js/index.js
+++ js/index.js
@@ -40,25 +40,37 @@ $("#zhaohui").click(function(){
     $(".ok").hide();
     $(".goregst").hide();
     $(".goregst-bottom").hide();
+    $("input").val("");
+    $("label.error").css("display","none");
 });
 //确认密码
 $("#back").click(function(){
-    $(".goLogin-bottom").hide();
-    $(".goLogin").hide();
-    $(".goForget").hide();
-    $(".ok").show();
-    $(".goregst").hide();
-    $(".goregst-bottom").hide();
+
+    // $(".goLogin-bottom").show();
+    // $(".goLogin").show();
+    // $(".goregst").hide();
+    // $(".goregst-bottom").hide();
+    // $(".goForget").hide();
+    // $(".ok").hide();
+    // $("input").val("");
+    // $("label.error").css("display","none");
+
+    // $(".goLogin-bottom").hide();
+    // $(".goLogin").hide();
+    // $(".goForget").hide();
+    // $(".ok").show();
+    // $(".goregst").hide();
+    // $(".goregst-bottom").hide();
 });
-//确认密码之后去登录
-$("#denglu").click(function(){
-    $(".goLogin-bottom").show();
-    $(".goLogin").show();
-    $(".goregst").hide();
-    $(".goregst-bottom").hide();
-    $(".goForget").hide();
-    $(".ok").hide();
-});
+// //确认密码之后去登录
+// $("#denglu").click(function(){
+//     $(".goLogin-bottom").show();
+//     $(".goLogin").show();
+//     $(".goregst").hide();
+//     $(".goregst-bottom").hide();
+//     $(".goForget").hide();
+//     $(".ok").hide();
+// });
 // --------------------------------------------------------------------------------------------
 // $(document).ready(function(){
 //     $("#loginForm").validate({
@@ -178,7 +190,7 @@ $(function(){
 
     });
     //注册的表单
-
+                    //判断是否被注册 获取验证码
     $("#getYan").click(function(){
         var oRegEmail = $('[name=regEmail]').val();
         $.ajax({ //jQuery中的ajax方法
@@ -277,75 +289,151 @@ $(function(){
                 equalTo:"两次密码输入不一致"
             }
         },
+
+    });
+    //找回密码
+
+    $("#forgetYan").click(function(){
+        var oForgetEmail = $('[name=forgetEmail]').val();
+        $.ajax({ //jQuery中的ajax方法
+            type: "POST",
+            url: "user/checkRebackCode.action",
+            data:({
+                email:oForgetEmail,
+            }),
+            dataType: "json",//数据类型是json
+            success: function (data) {//如果成功获得了值执行的方法，目的是为了让用户知道执行的操作成功了。
+                console.log(data);
+                if(!data){
+                    $("#forgetRegist").css("display","block");
+                }else{
+                    $("#forgetRegist").css("display","none");
+                }
+            },
+            error: function (jqXHR) {//失败后执行的方法。
+                console.log(jqXHR)
+            },
+        })
+    })
+
+    $("#forgetForm").validate({
+        debug: true, //调试模式取消submit的默认提交功能
+        //errorClass: "label.error", //默认为错误的样式类为：error
+        focusInvalid: false, //当为false时，验证无效时，没有焦点响应
+        onkeyup: false,
+        submitHandler: function(form){   //表单提交句柄,为一回调函数，带一个参数：form
+            var oForgetEmail = $('[name=forgetEmail]').val();
+            var oForgetpwd = $('[name=forgetpwd]').val();
+            var oForgetYan =$('[name=forgetYan]').val();
+            console.log(oForgetEmail);
+            console.log(oForgetpwd);
+            console.log(oForgetYan);
+            $.ajax({ //jQuery中的ajax方法
+                type: "POST",
+                url: "user/checkReback.action",
+                data:({
+                	email:oForgetEmail,
+                    newPassword:oForgetpwd,
+                    code:oForgetYan
+                }),
+                dataType: "json",//数据类型是json
+                success: function (data) {//如果成功获得了值执行的方法，目的是为了让用户知道执行的操作成功了。
+                    console.log(data)
+                    if(data){
+                        $("#cuowuYan").css("display","none");
+                        alert("修改成功！")
+                        $(".ok").hide();
+                        $("input").val("");
+                        $("label.error").css("display","none");
+                        
+                        $(".goLogin-bottom").show();
+                        $(".goLogin").show();
+                        $(".goregst").hide();
+                        $(".goregst-bottom").hide();
+                        $(".goForget").hide();
+                        $(".ok").hide();
+                        $("input").val("");
+                        $("input").removeClass("error")
+                        $("label.error").css("display","none");
+
+                    }else {
+                        $("#cuowuYan").css("display","block");
+                    }
+                },
+                error: function (jqXHR) {//失败后执行的方法。
+                    console.log(jqXHR)
+                },
+            })
+            // alert("提交表单");
+            // form.submit();   //提交表单
+        },
+        rules:{
+            forgetEmail:{
+                required:true,
+                email:true
+            },
+            forgetpwd:{
+                required:true,
+                rangelength:[6,10]
+            },
+            confirm_pwd:{
+                equalTo:"#forgetpwd"
+            }
+
+        },
+        messages:{
+            forgetEmail:{
+                required:"必填",
+                email:"E-Mail格式不正确"
+            },
+            forgetpwd:{
+                required: "不能为空",
+                rangelength: "密码最小长度:6, 最大长度:10。"
+            },
+            confirm_pwd:{
+                equalTo:"两次密码输入不一致"
+            }
+
+        },
         // success: function(label) {
         //     // label.removeClass("error").addClass("valid").text("Ok!")
 
         // }
 
     });
-    //找回密码
-    $("#rePassword").validate({
-        debug: true, //调试模式取消submit的默认提交功能
-        //errorClass: "label.error", //默认为错误的样式类为：error
-        focusInvalid: false, //当为false时，验证无效时，没有焦点响应
-        onkeyup: false,
-        submitHandler: function(form){   //表单提交句柄,为一回调函数，带一个参数：form
-            alert("提交表单");
-            form.submit();   //提交表单
-        },
-        rules:{
-            regEmail1:{
-                required:true,
-                email:true
-            }
-
-        },
-        messages:{
-            regEmail1:{
-                required:"必填",
-                email:"E-Mail格式不正确"
-            },
-
-        },
-        success: function(label) {
-            // label.removeClass("error").addClass("valid").text("Ok!")
-
-        }
-
-    });
     //重置密码
-    $("#reForm").validate({
-        debug: true, //调试模式取消submit的默认提交功能
-        //errorClass: "label.error", //默认为错误的样式类为：error
-        focusInvalid: false, //当为false时，验证无效时，没有焦点响应
-        onkeyup: false,
-        submitHandler: function(form){   //表单提交句柄,为一回调函数，带一个参数：form
-            alert("提交表单");
-            form.submit();   //提交表单
-        },
-        rules:{
-            repwd:{
-                required:true,
-                rangelength:[6,10]
-            },
-            confirm_password:{
-                equalTo:"#repwd"
-            }
-        },
-        messages:{
-            repwd:{
-                required: "不能为空",
-                rangelength: "密码最小长度:6, 最大长度:10。"
-            },
-            confirm_password:{
-                equalTo:"两次密码输入不一致"
-            }
-        },
-        success: function(label) {
-            // label.removeClass("error").addClass("valid").text("Ok!")
+    // $("#reForm").validate({
+    //     debug: true, //调试模式取消submit的默认提交功能
+    //     //errorClass: "label.error", //默认为错误的样式类为：error
+    //     focusInvalid: false, //当为false时，验证无效时，没有焦点响应
+    //     onkeyup: false,
+    //     submitHandler: function(form){   //表单提交句柄,为一回调函数，带一个参数：form
+    //         alert("提交表单");
+    //         form.submit();   //提交表单
+    //     },
+    //     rules:{
+    //         repwd:{
+    //             required:true,
+    //             rangelength:[6,10]
+    //         },
+    //         confirm_password:{
+    //             equalTo:"#repwd"
+    //         }
+    //     },
+    //     messages:{
+    //         repwd:{
+    //             required: "不能为空",
+    //             rangelength: "密码最小长度:6, 最大长度:10。"
+    //         },
+    //         confirm_password:{
+    //             equalTo:"两次密码输入不一致"
+    //         }
+    //     },
+    //     success: function(label) {
+    //         // label.removeClass("error").addClass("valid").text("Ok!")
 
-        }
+    //     }
 
-    });
+    // });
 
 });
