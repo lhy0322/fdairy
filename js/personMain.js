147af303a1d58925227ea1598974ffd834fa45be
HuaLouChuQiu <1015110571@qq.com>
2018-07-13 11:34:52 +0800

    1

---
+++ js/personMain.js
@@ -0,0 +1,20 @@
+$(function(){
+    //退出
+    $(".ok-exit").click(function(){
+        sessionStorage.removeItem("email");
+        $('#myModal2').modal('hide')
+        $("#name").css("display","none");
+        $("#goin1").css("display","block");
+    })
+
+    //session
+    var userEmail = sessionStorage.getItem("email");
+    console.log(userEmail);
+    if(userEmail != undefined){
+        $("#name-name").html(userEmail);
+        $("#name").css("display","block");
+        $("#goin1").css("display","none");
+    }else{
+        console.log("kong")
+    }
+})
