3ab928cfd227fefa39cac0ef6cc6b670b69eb28d
HuaLouChuQiu <1015110571@qq.com>
2018-07-13 11:51:01 +0800

    A

--- js/exit.js
+++ js/exit.js
@@ -1,12 +1,12 @@
 $(function(){
-    //退出
-    $(".ok-exit").click(function(){
-        sessionStorage.removeItem("email");
+    // //退出
+    // $(".ok-exit").click(function(){
+    //     sessionStorage.removeItem("email");
 
-        $('#myModal2').modal('hide')
-        $("#name").css("display","none");
-        $("#goin1").css("display","block");
-    })
+    //     $('#myModal2').modal('hide')
+    //     $("#name").css("display","none");
+    //     $("#goin1").css("display","block");
+    // })
 
     //session
     var userEmail = sessionStorage.getItem("email");
@@ -15,7 +15,7 @@ $(function(){
         $("#name-name").html(userEmail);
         $("#name").css("display","block");
         $("#goin1").css("display","none");
-        
+        $(".personal p").html(userEmail+"的小厨房");
     }else{
         console.log("kong")
     }
--- js/personMain.js
+++ js/personMain.js
@@ -8,16 +8,16 @@ $(function(){
     // })
 
     //session
-    var userEmail = sessionStorage.getItem("email");
-    console.log(userEmail);
-    if(userEmail != undefined){
-        $("#name-name").html(userEmail);
-        $("#name").css("display","block");
-        $("#goin1").css("display","none");
-        $(".personal p").html(userEmail+"的小厨房");
-    }else{
-        console.log("kong")
-    }
+    // var userEmail = sessionStorage.getItem("email");
+    // console.log(userEmail);
+    // if(userEmail != undefined){
+    //     $("#name-name").html(userEmail);
+    //     $("#name").css("display","block");
+    //     $("#goin1").css("display","none");
+    //     $(".personal p").html(userEmail+"的小厨房");
+    // }else{
+    //     console.log("kong")
+    // }
 
 
 })
