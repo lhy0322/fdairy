9c9964077c9e022633cfec259ff9eeabd1fbcece
HuaLouChuQiu <1015110571@qq.com>
2018-07-16 16:51:50 +0800

    a

--- js/index-box.js
+++ js/index-box.js
@@ -14,7 +14,7 @@ $(function () {
                     for (var i = 0; i < data.length; i++) {
                         let html =
                             `
-                    <div class="col-xs-6 col-sm-3 box">
+                    <div class="col-xs-6 col-sm-3 box item">
                     <div class="new-box">
                         <a href="main.html?shopId=${data[i].shop_id}">
                             <img src="${data[i].shop_img}" class="img-responsive images" alt="Responsive image">
@@ -36,16 +36,24 @@ $(function () {
                     `
                         $(place).append($(html).attr("style",""))
                     }
-
+                    $("#shopBox2").mpmansory(
+                        {
+                          childrenClass: '', // default is a div
+                          columnClasses: '', //add classes to items
+                          breakpoints:{
+                            lg: 3, 
+                            md: 4, 
+                            sm: 6,
+                            xs: 12
+                          },
+                          distributeBy: { order: false, height: false, attr: 'data-order', attrOrder: 'asc' }, 
+                          onload: function (items) {
+                            //make somthing with items
+                          } 
+                        }
+                      );
                 }
-                var $container = $('#shopBox1');
-                $container.imagesLoaded(function() {
-                    $container.masonry({
-                            itemSelector: '.box',
-                            gutter: 0,
-                            isAnimated: flase,
-                        });
-                 });
+                
             },
             error: function (jqXHR) {//失败后执行的方法。
                 console.log(jqXHR)
@@ -65,7 +73,7 @@ $(function () {
                         let html =
                             `
 
-                    <div class="col-xs-6 col-sm-3 box">
+                    <div class="col-xs-6 col-sm-3 box item">
                     <div class="new-box">
                     <a href="main.html?shopId=${data[i].shop_id}">
                         <img src="${data[i].shop_img}" class="img-responsive" alt="Responsive image">
@@ -85,16 +93,23 @@ $(function () {
                     </div>
                     `
                         $(place).append(html)
-
-                        var $container = $('#shopBox2');
-                        $container.imagesLoaded(function() {
-                            $container.masonry({
-                                    itemSelector: '.box',
-                                    gutter: 0,
-                                    isAnimated: flase,
-                                });
-                         });
                     }
+                    $("#shopBox2").mpmansory(
+                        {
+                          childrenClass: '', // default is a div
+                          columnClasses: '', //add classes to items
+                          breakpoints:{
+                            lg: 3, 
+                            md: 4, 
+                            sm: 6,
+                            xs: 12
+                          },
+                          distributeBy: { order: false, height: false, attr: 'data-order', attrOrder: 'asc' }, 
+                          onload: function (items) {
+                            //make somthing with items
+                          } 
+                        }
+                      );
                 }
             },
             error: function (jqXHR) {//失败后执行的方法。
@@ -102,49 +117,6 @@ $(function () {
             },
         })
     }
-
-    // var shopbox2 = function (url, place) {
-    //     $.ajax({ //jQuery中的ajax方法
-    //         type: "POST",
-    //         url: url,
-    //         dataType: "json",//数据类型是json
-    //         success: function (data) {//如果成功获得了值执行的方法，目的是为了让用户知道执行的操作成功了。
-
-    //             console.log(data);
-    //             if (data != '') {
-    //                 for (var i = 0; i < data.length; i++) {
-    //                     let html =
-    //                         `
-
-    //                 <div class="col-xs-6 col-sm-3">
-    //                 <div class="new-box">
-    //                 <a href="main.html?shopId=${data[i].shop_id}">
-    //                     <img src="${data[i].shop_img}" class="img-responsive" alt="Responsive image">
-    //                     <h5>${data[i].shop_name}</h5>
-    //                     <h6>新店开张，送代金券哦</h6>
-    //                     <h6>配送时间
-    //                         <span> ${data[i].s_time}分钟</span>
-    //                     </h6>
-    //                     <div class="shop">
-    //                         <strong class="color2 money">￥${data[i].ave_price}</strong>
-    //                         <small>已售
-    //                             <span> ${data[i].sold}</span>
-    //                         </small>
-    //                     </div>
-    //                 </a>
-    //                 </div>
-    //                 </div>
-    //                 `
-    //                     $(place).append(html)
-    //                 }
-    //             }
-    //         },
-    //         error: function (jqXHR) {//失败后执行的方法。
-    //             console.log(jqXHR)
-    //         },
-    //     })
-    // }
-
     var shopbox3 = function (url, place) {
         $.ajax({ //jQuery中的ajax方法
             type: "POST",
@@ -157,7 +129,7 @@ $(function () {
                     for (var i = 0; i < data.length; i++) {
                         let html =
                             `
-                    <div class="col-xs-6 col-sm-3 box">
+                    <div class="col-xs-6 col-sm-3 box item">
                     <div class="new-box">
                     <a href="main.html?shopId=${data[i].shop_id}">
                         <img src="${data[i].shop_img}" class="img-responsive" alt="Responsive image">
@@ -177,15 +149,23 @@ $(function () {
                     </div>
                     `
                         $(place).append(html)
-                        var $container = $('#shopBox3');
-                        $container.imagesLoaded(function() {
-                            $container.masonry({
-                                    itemSelector: '.box',
-                                    gutter: 0,
-                                    isAnimated: flase,
-                                });
-                         });
                     }
+                    $("#shopBox2").mpmansory(
+                        {
+                          childrenClass: '', // default is a div
+                          columnClasses: '', //add classes to items
+                          breakpoints:{
+                            lg: 3, 
+                            md: 4, 
+                            sm: 6,
+                            xs: 12
+                          },
+                          distributeBy: { order: false, height: false, attr: 'data-order', attrOrder: 'asc' }, 
+                          onload: function (items) {
+                            //make somthing with items
+                          } 
+                        }
+                      );
                 }
             },
             error: function (jqXHR) {//失败后执行的方法。
