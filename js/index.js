ec448157f22bf2f31949866eb4f77899b9604b65
HuaLouChuQiu <1015110571@qq.com>
2018-07-17 09:26:22 +0800

    aaa

--- js/index.js
+++ js/index.js
@@ -364,39 +364,5 @@ $(function(){
         // }
 
     });
-    //重置密码
-    // $("#reForm").validate({
-    //     debug: true, //调试模式取消submit的默认提交功能
-    //     //errorClass: "label.error", //默认为错误的样式类为：error
-    //     focusInvalid: false, //当为false时，验证无效时，没有焦点响应
-    //     onkeyup: false,
-    //     submitHandler: function(form){   //表单提交句柄,为一回调函数，带一个参数：form
-    //         alert("提交表单");
-    //         form.submit();   //提交表单
-    //     },
-    //     rules:{
-    //         repwd:{
-    //             required:true,
-    //             rangelength:[6,10]
-    //         },
-    //         confirm_password:{
-    //             equalTo:"#repwd"
-    //         }
-    //     },
-    //     messages:{
-    //         repwd:{
-    //             required: "不能为空",
-    //             rangelength: "密码最小长度:6, 最大长度:10。"
-    //         },
-    //         confirm_password:{
-    //             equalTo:"两次密码输入不一致"
-    //         }
-    //     },
-    //     success: function(label) {
-    //         // label.removeClass("error").addClass("valid").text("Ok!")
-
-    //     }
-
-    // });
 
 });
